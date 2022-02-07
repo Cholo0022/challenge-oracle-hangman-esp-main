b8248fd88e116edd39300808e2f9dee21de89b30
Cholo0022 <andres.bozzani@gmail.com>
2022-02-07 16:47:53 -0300

    Logica de ganador/perdedor casi terminada

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -1,38 +1,3 @@
-function palabraSecreta() {
-  var listadoPalabras = ["ALURA", "ORACLE", "JAVASCRIPT", "HTML"];
-  var indice = Math.round(Math.random() * (listadoPalabras.length - 1));
-  var palabraAlAzar = listadoPalabras[indice];
-  return palabraAlAzar;
-}
-
-function verificarTeclaPresionada() {
-  var palabra = palabraSecreta();
-  var intentos = 0;
-  console.log(palabra)
-  var patron = /^[A-ZÑ\s]+$/;
-  window.addEventListener("keydown", function (event) {
-    event.preventDefault();
-    var letraValida = false;
-    if (patron.test(event.key)) {
-      for (var i = 0; i < palabra.length; i++) {
-        if (palabra[i] === event.key) {
-          letraValida = true;
-          console.log("La letra " + event.key + " si está en la posición " + i);
-          dibujarLetraCorrecta(event.key)
-        }        
-      }
-    }
-    if (!letraValida) {
-      dibujarLetraIncorrecta(event.key)
-      intentos += 1;
-      console.log(intentos)
-      dibujarHorca(intentos)
-    }
-    return letraValida; 
-  });
-}
-
-
 function inicioJuego() {
   var btnInicioJuego = document.querySelector("#iniciar-juego");
 
@@ -45,4 +10,48 @@ function inicioJuego() {
 }
 inicioJuego();
 
+function palabraSecreta() {
+  var listadoPalabras = ["ALURA", "ORACLE", "JAVASCRIPT", "HTML"];
+  var indice = Math.round(Math.random() * (listadoPalabras.length - 1));
+  var palabraAlAzar = listadoPalabras[indice];
+  dibujarLineas(palabraAlAzar.length);
+  return palabraAlAzar;
+}
 
+function verificarTeclaPresionada() {
+  var palabra = palabraSecreta();
+  var intentos = 0;
+  var largoPalabra = 0;
+  console.log(palabra);
+  var patron = /^[A-ZÑ]+$/;
+  window.addEventListener("keydown", function (event) {
+    event.preventDefault();
+    var letraValida = false;
+    if (patron.test(event.key)) {
+      for (var i = 0; i < palabra.length; i++) {
+        if (palabra[i] === event.key) {
+          largoPalabra += 1;
+          letraValida = true;
+          console.log("La letra " + event.key + " si está en la posición " + i);
+          dibujarLetraCorrecta(event.key, i);
+          if (largoPalabra == palabra.length) {
+            dibujarGanaste();
+          }
+        }
+      }
+    }
+    if (!letraValida) {
+      dibujarLetraIncorrecta(event.key, intentos);
+      intentos += 1;
+      dibujarHorca(intentos);
+      finDelJuego(intentos);
+    }
+    return letraValida;
+  });
+}
+
+function finDelJuego(intentos) {
+  if (intentos == 9) {
+    dibujarFinDelJuego();
+  }
+}
--- js/canvas.js
+++ js/canvas.js
@@ -1,124 +1,166 @@
-
 function dibujarTablero(x) {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
   pincel.fillStyle = "lightgrey";
   pincel.fillRect(0, 0, 1200, 800);
-  for (var x = 50; x < 250; x++){
-    pincel.fillStyle = "black";   //
+  for (var x = 50; x < 250; x++) {
+    pincel.fillStyle = "black"; //
     pincel.beginPath();
-    pincel.arc(x, 780, 5 ,0 , 2*3.14);
+    pincel.arc(x, 780, 5, 0, 2 * 3.14);
     pincel.fill();
   }
   return pincel;
-}
-
-function dibujarLetraCorrecta(letraCorrecta) {
-  var pantalla = document.querySelector("#ahorcado");
-  var pincel = pantalla.getContext("2d");
-
-  pincel.fillStyle = "blue";
-  pincel.font = "bold 25px Arial";
-  pincel.fillText(letraCorrecta, 1000, 50);
-  pincel.textAlign = "right";
-}
-
-function dibujarLetraIncorrecta(letraIncorrecta) {
-  var pantalla = document.querySelector("#ahorcado");
-  var pincel = pantalla.getContext("2d");
-
-  pincel.fillStyle = "red";
-  pincel.font = "bold 25px Arial";
-  pincel.fillText(letraIncorrecta, 1000, 300);
-  pincel.textAlign = "left";
-
-  dibujarHorca()
 }
 
 function dibujarHorca(intentos) {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
   //error 1
-  if (intentos == 1){
-      for (var y = 780; y > 200; y--){
-        pincel.fillStyle = "black"; 
-        pincel.beginPath();
-        pincel.arc(150, y, 5 ,0 , 2*3.14);
-        pincel.fill();
+  if (intentos == 1) {
+    for (var y = 780; y > 200; y--) {
+      pincel.fillStyle = "black";
+      pincel.beginPath();
+      pincel.arc(150, y, 5, 0, 2 * 3.14);
+      pincel.fill();
     }
   }
-    //error 2
-    if (intentos == 2){
-    for (var x = 150; x < 400; x++){
-      pincel.fillStyle = "black"; 
+  //error 2
+  if (intentos == 2) {
+    for (var x = 150; x < 400; x++) {
+      pincel.fillStyle = "black";
       pincel.beginPath();
-      pincel.arc(x, 200, 5 ,0 , 2*3.14);
+      pincel.arc(x, 200, 5, 0, 2 * 3.14);
       pincel.fill();
     }
   }
   //error 3
-  if (intentos == 3){
-    for (var y = 200; y < 300; y++){
-      pincel.fillStyle = "black"; 
+  if (intentos == 3) {
+    for (var y = 200; y < 300; y++) {
+      pincel.fillStyle = "black";
       pincel.beginPath();
-      pincel.arc(400, y, 5 ,0 , 2*3.14);
+      pincel.arc(400, y, 5, 0, 2 * 3.14);
       pincel.fill();
     }
   }
-    //error4
-    if (intentos == 4){
-    pincel.fillStyle = "black";   
-      pincel.beginPath();
-      pincel.arc(400, 300, 50 ,0 , 2*3.14);
+  //error4
+  if (intentos == 4) {
+    pincel.fillStyle = "black";
+    pincel.beginPath();
+    pincel.arc(400, 300, 50, 0, 2 * 3.14);
     pincel.fill();
-    pincel.fillStyle = "lightgrey";   
-      pincel.beginPath();
-      pincel.arc(400, 300, 45 ,0 , 2*3.14);
-      pincel.fill();
-    }
+    pincel.fillStyle = "lightgrey";
+    pincel.beginPath();
+    pincel.arc(400, 300, 45, 0, 2 * 3.14);
+    pincel.fill();
+  }
   //error 5
-  if (intentos == 5){
+  if (intentos == 5) {
     for (var y = 350; y < 550; y++) {
       pincel.fillStyle = "black";
       pincel.beginPath();
       pincel.arc(400, y, 5, 0, 2 * 3.14);
-      pincel.fill();  
+      pincel.fill();
     }
   }
-    //error 6 brazo izq
-    if (intentos == 6){
+  //error 6 brazo izq
+  if (intentos == 6) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
     pincel.beginPath();
     pincel.moveTo(250, 350);
     pincel.lineTo(400, 450);
     pincel.stroke();
-    }
-    //error 7 brazo der
-    if (intentos == 7){
+  }
+  //error 7 brazo der
+  if (intentos == 7) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
     pincel.beginPath();
     pincel.moveTo(400, 450);
     pincel.lineTo(550, 350);
     pincel.stroke();
-    }
-    //error 8 pierna izq
-    if (intentos == 8){
+  }
+  //error 8 pierna izq
+  if (intentos == 8) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
     pincel.beginPath();
     pincel.moveTo(400, 550);
     pincel.lineTo(300, 700);
     pincel.stroke();
-    }
-    //error 8 pierna der
-    if (intentos == 9){
+  }
+  //error 8 pierna der
+  if (intentos == 9) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
     pincel.beginPath();
     pincel.moveTo(400, 550);
     pincel.lineTo(500, 700);
     pincel.stroke();
-    }
   }
+}
+
+function dibujarFinDelJuego() {
+  var pantalla = document.querySelector("#ahorcado");
+  var pincel = pantalla.getContext("2d");
+
+  pincel.fillStyle = "red";
+  pincel.font = "bold 50px Arial";
+  pincel.fillText("Fin del Juego...Perdiste!!!", 500, 500);
+  pincel.textAlign = "left";
+}
+
+function dibujarGanaste() {
+  var pantalla = document.querySelector("#ahorcado");
+  var pincel = pantalla.getContext("2d");
+
+  pincel.fillStyle = "blue";
+  pincel.font = "bold 50px Arial";
+  pincel.fillText("Felicidades ganaste!!!", 900, 500);
+  pincel.textAlign = "left";
+}
+
+function dibujarLineas(cantidad) {
+  var pantalla = document.querySelector("#ahorcado");
+  var pincel = pantalla.getContext("2d");
+
+  for (var x = 600; x < cantidad * 40 + 600; x = x + 40) {
+    pincel.lineWidth = 5;
+    pincel.strokeStyle = "black";
+    pincel.beginPath();
+    pincel.moveTo(x, 780);
+    pincel.lineTo(x + 30, 780);
+    pincel.stroke();
+  }
+}
+
+function dibujarLetraCorrecta(letraCorrecta, posicion) {
+  var pantalla = document.querySelector("#ahorcado");
+  var pincel = pantalla.getContext("2d");
+
+  if (posicion < 1) {
+    pincel.fillStyle = "blue";
+    pincel.font = "bold 25px Arial";
+    pincel.fillText(letraCorrecta, posicion * 40 + 610, 775);
+    pincel.textAlign = "right";
+  } else {
+    pincel.fillStyle = "blue";
+    pincel.font = "bold 25px Arial";
+    pincel.fillText(letraCorrecta, posicion * 40 + 625, 775);
+    pincel.textAlign = "right";
+  }
+
+  return posicion;
+}
+
+function dibujarLetraIncorrecta(letraIncorrecta, posicion) {
+  var pantalla = document.querySelector("#ahorcado");
+  var pincel = pantalla.getContext("2d");
+
+  pincel.fillStyle = "red";
+  pincel.font = "bold 25px Arial";
+  pincel.fillText(letraIncorrecta, posicion * 40 + 625, 300);
+  pincel.textAlign = "left";
+
+  dibujarHorca();
+}
