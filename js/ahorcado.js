72221b155e96ca4e16703c31d118bb5420741b9f
Cholo0022 <andres.bozzani@gmail.com>
2022-02-08 22:36:29 -0300

    Juego terminado, faltan ajustes en extra de agregar palabra

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -1,19 +1,27 @@
+//lISTADO DE PALABRA PARA ELEJIR AL AZAR
+var listadoPalabras = ["ALURA", "ORACLE", "JAVASCRIPT", "HTML"];
+console.log(listadoPalabras)
+
+//Iniciando el juego
 function inicioJuego() {
   var btnInicioJuego = document.querySelector("#iniciar-juego");
 
   btnInicioJuego.addEventListener("click", function () {
     console.log("Iniciando juego");
+    //Cuando comienza nos posicionamos en el tablero
+    nivel.scrollIntoView();
     palabraSecreta();
     dibujarTablero();
     verificarTeclaPresionada();
+    
   });
   return;
 }
+//llamando a la función para comenzar el juego
 inicioJuego();
-//agregarPalabra();
+agregarPalabra();
 
-var listadoPalabras = ["ALURA", "ORACLE", "JAVASCRIPT", "HTML"];
-
+//Función que elije al azar palabras de un array
 function palabraSecreta() {
   var indice = Math.round(Math.random() * (listadoPalabras.length - 1));
   var palabraAlAzar = listadoPalabras[indice];
@@ -32,42 +40,48 @@ function agregarPalabra() {
       listadoPalabras.push(palabraNueva);
       console.log(listadoPalabras);
     }
+    palabraNueva = "";
   });
   return;
 }
 
+//Función que verifica si la tecla presionada.
 function verificarTeclaPresionada() {
   var palabra = palabraSecreta();
   var intentos = 0;
   var largoPalabra = 0;
   console.log(palabra);
-  //var patron = new RegExp("[A-Z]+");
   var patron = /^[A-ZÑ\s]+$/;
   var letraRepetida = [];
-
+  //Escuchamos el teclado
   window.addEventListener("keydown", function (event) {
     var repetida = false;
     var letraValida = false;
+    //verificamos si la tecla ya ha sido presionada y la marcamos como verdadera si así fue.
     for (var x = 0; x < letraRepetida.length; x++) {
       if (letraRepetida[x] === event.key) {
-        console.log("Letra " + event.key + " repetida");
+        //console.log("Letra " + event.key + " repetida");
         repetida = true;
       }
     }
+    //Si la tecla presionada es mayuscula y no está marcada como true no se repitió e ingresa.
     if (patron.test(event.key) && !repetida) {
-      //repetida = false;
       letraRepetida.push(event.key);
+      //Recorremos la palabra elejida al azar y comparamos con la letra tecleada, si coincide se dibuja la letra correcta.
       for (var i = 0; i < palabra.length; i++) {
         if (palabra[i] === event.key) {
           largoPalabra += 1;
           letraValida = true;
-          console.log("La letra " + event.key + " si está en la posición " + i);
+          //console.log("La letra " + event.key + " si está en la posición " + i);
           dibujarLetraCorrecta(event.key, i);
+          //Si el largo de la palabra coincide con el contador largoPalabra quiere decir que ganamos y llamamos a dicha función.
           if (largoPalabra == palabra.length) {
             dibujarGanaste();
           }
         }
       }
+      //Si no hay coincidencias entre la palabra elejida al azar y la tecleada, entonces llamamos a la función que dibuje letra incorrecta,
+      //dibujar horca y fin del juego
       if (!letraValida) {
         dibujarLetraIncorrecta(event.key, intentos);
         intentos += 1;
@@ -75,11 +89,12 @@ function verificarTeclaPresionada() {
         finDelJuego(intentos);
       }
     }
-    console.log(letraRepetida);
+    //console.log(letraRepetida);
     return;
   });
 }
 
+//Función que recibe la cantidad de intentos y en caso de llegar al último intento llama a dibujar el mensaje final.
 function finDelJuego(intentos) {
   if (intentos == 9) {
     dibujarFinDelJuego();
--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,4 @@
+//Función que dibuja el tablero 
 function dibujarTablero(x) {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
@@ -12,10 +13,11 @@ function dibujarTablero(x) {
   return pincel;
 }
 
+//Función que va dibujando la horca a medida erran la letra a coincidir
 function dibujarHorca(intentos) {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
-  //error 1
+  // Dibuja el primer error de la horca
   if (intentos == 1) {
     for (var y = 780; y > 200; y--) {
       pincel.fillStyle = "black";
@@ -24,7 +26,7 @@ function dibujarHorca(intentos) {
       pincel.fill();
     }
   }
-  //error 2
+  // Dibuja el segundo error de la horca
   if (intentos == 2) {
     for (var x = 150; x < 400; x++) {
       pincel.fillStyle = "black";
@@ -33,7 +35,7 @@ function dibujarHorca(intentos) {
       pincel.fill();
     }
   }
-  //error 3
+  // Dibuja el tercer error de la horca
   if (intentos == 3) {
     for (var y = 200; y < 300; y++) {
       pincel.fillStyle = "black";
@@ -42,7 +44,7 @@ function dibujarHorca(intentos) {
       pincel.fill();
     }
   }
-  //error4
+  // Dibuja el cuarto error de la horca
   if (intentos == 4) {
     pincel.fillStyle = "black";
     pincel.beginPath();
@@ -53,7 +55,7 @@ function dibujarHorca(intentos) {
     pincel.arc(400, 300, 45, 0, 2 * 3.14);
     pincel.fill();
   }
-  //error 5
+  // Dibuja el quinto error de la horca
   if (intentos == 5) {
     for (var y = 350; y < 550; y++) {
       pincel.fillStyle = "black";
@@ -62,7 +64,7 @@ function dibujarHorca(intentos) {
       pincel.fill();
     }
   }
-  //error 6 brazo izq
+  // Dibuja el sexto error de la horca
   if (intentos == 6) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
@@ -71,7 +73,7 @@ function dibujarHorca(intentos) {
     pincel.lineTo(400, 450);
     pincel.stroke();
   }
-  //error 7 brazo der
+  // Dibuja el septimo error de la horca
   if (intentos == 7) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
@@ -80,7 +82,7 @@ function dibujarHorca(intentos) {
     pincel.lineTo(550, 350);
     pincel.stroke();
   }
-  //error 8 pierna izq
+  // Dibuja el octavo error de la horca
   if (intentos == 8) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
@@ -89,7 +91,7 @@ function dibujarHorca(intentos) {
     pincel.lineTo(300, 700);
     pincel.stroke();
   }
-  //error 8 pierna der
+  // Dibuja el último error de la horca
   if (intentos == 9) {
     pincel.lineWidth = 10;
     pincel.strokeStyle = "black";
@@ -98,13 +100,13 @@ function dibujarHorca(intentos) {
     pincel.lineTo(500, 700);
     pincel.stroke();
   }
+  //Al presionar una tecla recargo la pagina para terminar el juego.
   if (intentos >= 10) {
     location.reload();
   }
-
-  return;
 }
 
+//Función que dibuja el mensaje cuando se pierde el juego
 function dibujarFinDelJuego() {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
@@ -119,6 +121,7 @@ function dibujarFinDelJuego() {
   return;
 }
 
+//Función que dibuja el mensaje de ganador
 function dibujarGanaste() {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
@@ -130,13 +133,14 @@ function dibujarGanaste() {
   pincel.fillStyle = "black";
   pincel.font = "bold 15px Arial";
   pincel.fillText("Precione cualquier tecla para terminar", 500, 550);
+  //Al tocar cualquier tecla recarga la pagina para volver a empezar
   window.addEventListener("keydown", function (event) {
     event.preventDefault();
     location.reload();
   });
-  return;
 }
 
+//Función que dibuja las lineas de la palabra elejida al azar
 function dibujarLineas(cantidad) {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
@@ -151,24 +155,27 @@ function dibujarLineas(cantidad) {
   }
 }
 
+//Función que va dibujando las letras acertadas en la posición correspondiente.
 function dibujarLetraCorrecta(letraCorrecta, posicion) {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
-
+  console.log(letraCorrecta);
+  console.log(posicion);
+   if (posicion >= 1) {
+    pincel.fillStyle = "blue";
+    pincel.font = "bold 25px Arial";
+    pincel.fillText(letraCorrecta, posicion * 40 + 625, 775);
+    pincel.textAlign = "right";
+  }
   if (posicion == 0) {
     pincel.fillStyle = "blue";
     pincel.font = "bold 25px Arial";
     pincel.fillText(letraCorrecta, posicion * 40 + 607, 775);
     pincel.textAlign = "right";
   }
-  if (posicion >= 1) {
-    pincel.fillStyle = "blue";
-    pincel.font = "bold 25px Arial";
-    pincel.fillText(letraCorrecta, posicion * 40 + 625, 775);
-    pincel.textAlign = "right";
-  }
 }
 
+//Función que va dibujando la letra incorrecta.
 function dibujarLetraIncorrecta(letraIncorrecta, posicion) {
   var pantalla = document.querySelector("#ahorcado");
   var pincel = pantalla.getContext("2d");
@@ -177,7 +184,4 @@ function dibujarLetraIncorrecta(letraIncorrecta, posicion) {
   pincel.font = "bold 25px Arial";
   pincel.fillText(letraIncorrecta, posicion * 40 + 625, 300);
   pincel.textAlign = "left";
-
-  dibujarHorca();
-  return;
 }
